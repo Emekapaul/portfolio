d760c9ae38bc6f8498763a1004a89a23faed75e2
Paul Nnaemeka <emekapaul302@gmail.com>
2025-04-02 01:00:52 +0100

    Create tailwind config file

---
+++ tailwind.config.mjs
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "var(--font-inter)",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        orbitron: ["var(--font-orbitron)"],
+        rajdhani: ["var(--font-rajdhani)"],
+        spaceMono: ["var(--font-space-mono)"],
+        poppins: ["var(--font-poppins)"],
+        montserrat: ["var(--font-montserrat)"],
+        pacifico: ["var(--font-pacifico)"],
+        playfair: ["var(--font-playfair)"],
+      },
+      colors: {
+        primary: {
+          50: "#f0f9ff",
+          100: "#e0f2fe",
+          200: "#bae6fd",
+          300: "#7dd3fc",
+          400: "#38bdf8",
+          500: "#0ea5e9",
+          600: "#0284c7",
+          700: "#0369a1",
+          800: "#075985",
+          900: "#0c4a6e",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
